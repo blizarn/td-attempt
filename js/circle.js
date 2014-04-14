b851d5c44721df8f432cf3b5d44e04e92b20dcf6
blizarn <cerril54@gmail.com>
2014-04-14 00:12:04 -0500

    Variable changes

--- js/circle.js
+++ js/circle.js
@@ -4,11 +4,12 @@ var circle = function(x, y, radius, path) {
 	this.radius = radius;
 	this.path = path;
 	this.index = 0;
+	this.velocity = 2;
 	this.draw = function() {
 		ctx.save();
 		ctx.translate(this.x, this.y);
 		ctx.fillStyle = "#FF0000";
-		ctx.fillRect(0, 0, 32, 32);
+		ctx.fillRect(0, 0, 31, 31);
 		ctx.restore();
 	};
 	this.update = function(canvas) {
@@ -19,15 +20,15 @@ var circle = function(x, y, radius, path) {
 		} else {
 			if (this.x !== (this.path[this.index][0]-1)*32) {
 				if (this.x > (this.path[this.index][0]-1)*32) {
-					this.x -= 1;
+					this.x -= this.velocity;
 				} else {
-					this.x += 1;
+					this.x += this.velocity;
 				}
 			} else if (this.y !== (this.path[this.index][1]-1)*32) {
 				if (this.y > (this.path[this.index][1]-1)*32) {
-					this.y -= 1;
+					this.y -= this.velocity;
 				} else {
-					this.y += 1;
+					this.y += this.velocity;
 				}
 			}
 		}
